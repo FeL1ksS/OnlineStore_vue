3feb8bcc8639fcc052bf0f708a1df989ed99e019
FeL1ksS <fel1kssx@gmail.com>
2017-10-09 21:09:19 +0300

    start files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+  @import '../node_modules/normalize.css';
+</style>
