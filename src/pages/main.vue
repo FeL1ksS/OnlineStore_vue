bde204dbca161b7a4ff4064b28e59d4f01e684c0
FeL1ksS <fel1kssx@gmail.com>
2017-10-12 02:50:00 +0300

    header intro

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="page">
         <header class="header">
-            <div class="header__introduction">
-                <p>Newest Shop</p>
-                <router-link to="/catalog">Переход</router-link>
+            <div class="header__intro">
+                <h1>OLIOS</h1>
+                <p>NEWEST FURNITURE SHOP</p>
+                <p class="header__intro-bg-text">NEWEST</p>   
+                <!-- <router-link to="/catalog"> -->
+                    <button class="header__intro-button"
+                        onclick="location.href='/catalog'">
+                        VIEW MORE
+                    </button>
+                <!-- </router-link> -->
             </div>
             <div class="header__content">
                 <div class="header__content-item">1</div>
@@ -23,23 +30,68 @@ export default {
 </script>
     
 <style scoped>
+    @font-face {
+        font-family: Lato-Thin;
+        src: url('../assets/font/Lato-Thin.ttf');
+    }
+    @font-face {
+        font-family: Lato-Black;
+        src: url('../assets/font/Lato-Black.ttf');
+    }
     .header{
         height: inherit;
         display: flex;
         flex-direction: column;
         justify-content: center;
-
         /* border: 1px solid black; */
     }
-    .header__introduction{
+    .header__intro{
+        position: relative;
+        font-size: 8vmin;
+        text-align: center;
+        font-family: "Lato-Thin";
+        
 
-        border: 1px solid black;
+        z-index: 10;
+        /* border: 1px solid black; */
     }
-    .header__content{
+    .header__intro h1,p{
+        margin: 0;
+    }
+    .header__intro-bg-text{
+        position: absolute;
+        top: 0; left: 0; bottom: 0; right: 0;
+        font-size: 32vmin;
+
+        margin: auto;
+        overflow:hidden; 
+
+        /* position:absolute; top:0; text-align:center; ; */
+
+        opacity: .03;
+        z-index: -1;
+
+        font-family: "Lato-Black";
+    }
+    .header__intro-button{
+        height: 42px;
+        width: 144px;
+
+        color: white;
+        font-family: "Lato-Black";
+        font-size: 12px;
+        letter-spacing: 1px;
+        background-color: #0023ff;        
+        border-radius: 40px;
+        border: none;
+        outline: none;
+        cursor: pointer;
+    }
+    
+    /* .header__content{
         width: 50%;
         align-self: center;
         display: inherit;
-        /* align-items: flex-end; */
         flex-wrap: wrap;
         justify-content: space-around;
         border: 1px solid black;
@@ -49,7 +101,6 @@ export default {
         height: 200px;
         margin: 10px;
         border: 1px solid black;
-        /* flex-grow: 1; */
     }
     .header__content-item:nth-child(1){
         -webkit-order: 0;
@@ -61,5 +112,6 @@ export default {
         -webkit-align-self: baseline;
         -ms-flex-item-align: baseline;
         align-self: baseline;
-    }
+    } */
+
 </style>
